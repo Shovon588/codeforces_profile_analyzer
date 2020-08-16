04aa9e0bc9f3779bc2b931ade5f0dc1a9c58bfe1
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-08-17 03:48:31 +0600

    View added for single search

--- cf_profile_analyzer/analyzer/views.py
+++ cf_profile_analyzer/analyzer/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
 from . utils import *
 
 # Create your views here.
 
 
 def single_profile_search(request):
-    data = None
+
+    data = ''
+    contest_info = ''
+    submission_info = ''
     if request.method == 'POST':
         handle = request.POST['handle']
         data = get_user_info(handle)
+        contest_info = get_contest_info(handle)
+        submission_info = get_submission_info(handle)
+        
+        if len(data)==0:
+            messages.error(request, "Invalid user handle. Please provide a valid one.")
 
-    return render(request, 'analyzer/single.html', data)
+    context = {'data': data, 'contest_info': contest_info, 'submission_info': submission_info}
+
+    return render(request, 'analyzer/single_search.html', context=context)
 
 
 def single_profile_detail(request):
