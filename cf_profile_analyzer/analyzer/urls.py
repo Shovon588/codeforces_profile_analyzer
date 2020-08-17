7403cb076cb373659afd5a1db47242837be2729d
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-08-18 01:15:18 +0600

    Unnecessary urls removed

--- cf_profile_analyzer/analyzer/urls.py
+++ cf_profile_analyzer/analyzer/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.single_profile_search, name='single_search'),
-    path('single-result', views.single_profile_detail, name='single_result'),
-    path('dual-search', views.dual_profile_search, name='dual_search'),
-    path('dual-result', views.dual_profile_detail, name='dual_result')
+    path('', views.single, name='single'),
+    path('dual', views.dual, name='dual')
 ]
