dd229093f65a2972e7e00422328c738401c4b2ee
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-08-19 01:34:40 +0600

    Ready for production

--- cf_profile_analyzer/cf_profile_analyzer/settings.py
+++ cf_profile_analyzer/cf_profile_analyzer/settings.py
@@ -25,9 +25,9 @@ TEMP_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = '1+g%(exxnb#pwmaum-8ld*ih-mw_4=wihs-=4%2qnv&_2&+bi9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'cfpa.pythonanywhere.com']
+ALLOWED_HOSTS = ['cfpa.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
