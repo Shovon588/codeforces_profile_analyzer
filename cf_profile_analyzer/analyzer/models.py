7594b61ecbe3d37313acbe02d758b033cf7b2abf
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-08-25 01:55:50 +0600

    Comparing model added

--- cf_profile_analyzer/analyzer/models.py
+++ cf_profile_analyzer/analyzer/models.py
@@ -2,8 +2,21 @@ from django.db import models
 
 
 class UserName(models.Model):
-    username = models.CharField(max_length=64, unique=True)
+    username = models.CharField(max_length=64)
     date = models.DateTimeField(auto_now_add=True)
+    host = models.CharField(max_length=155, blank=True, null=True)
+    ip_address = models.CharField(max_length=155, blank=True, null=True)
 
     def __str__(self):
         return self.username
+
+
+class Compare(models.Model):
+    user1 = models.CharField(max_length=64)
+    user2 = models.CharField(max_length=64)
+    date = models.DateTimeField(auto_now_add=True)
+    host = models.CharField(max_length=155)
+    ip_address = models.CharField(max_length=155)
+
+    def __str__(self):
+        return "Compared: %s with %s" %(self.user1, self.user2)
